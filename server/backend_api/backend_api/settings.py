d19941226779c586b6582b5d742b4d624582f268
Piotrek K <kpiotrek3@gmail.com>
2020-03-31 12:01:27 +0200

    Możliwość przesłania pliku na serwer

--- server/backend_api/backend_api/settings.py
+++ server/backend_api/backend_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'process'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Path to storage of uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- server/backend_api/process/views.py
+++ server/backend_api/process/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.core.files.storage import FileSystemStorage
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'process/index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'process/index.html')
